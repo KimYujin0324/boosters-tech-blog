8f96fa658057f666424680f58dcb16df21ab08ec
KimYujin0324 <yujin.kim@boosters.kr>
2024-07-09 18:15:39 +0900

    feat: commit

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -39,3 +39,36 @@
 $(document.links).filter(function() {
 return this.hostname != window.location.hostname;
 }).attr('target', '_blank');
+
+$(document).ready(function() {
+  // 태그 클릭 이벤트 처리
+  $('.bsts-tag-list a').on('click', function(e) {
+    e.preventDefault();
+    var tag = $(this).data('tag');
+
+    // 선택한 태그를 클래스 토글
+    $(this).toggleClass('selected');
+
+    // 선택된 태그 목록 생성
+    var selectedTags = [];
+    $('.bsts-tag-list a.selected').each(function() {
+      selectedTags.push($(this).data('tag'));
+    });
+
+    // 포스트 필터링
+    filterPosts(selectedTags);
+  });
+
+  function filterPosts(tags) {
+    $('.post').each(function() {
+      var postTags = $(this).data('tags').split(' ');
+
+      // 선택된 태그가 하나라도 포함되어 있으면 표시, 아니면 숨김
+      if (tags.length === 0 || tags.some(tag => postTags.includes(tag))) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  }
+});
