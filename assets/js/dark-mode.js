2259d5c16842229ce888e04152449d96f680c030
KimYujin0324 <yujin.kim@boosters.kr>
2024-07-11 16:48:42 +0900

    feat: commit

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -23,24 +23,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const sunIcon = document.getElementById('sun-icon');
   const moonIcon = document.getElementById('moon-icon');
-  const currentTheme = localStorage.getItem('theme');
 
-  if (currentTheme) {
-    document.body.classList.add(currentTheme);
+  if (sunIcon && moonIcon) {
+      sunIcon.addEventListener('click', () => {
+          document.body.classList.remove('dark-mode');
+          document.body.classList.add('light-mode');
+          localStorage.setItem('theme', 'light-mode');
+      });
+
+      moonIcon.addEventListener('click', () => {
+          document.body.classList.remove('light-mode');
+          document.body.classList.add('dark-mode');
+          localStorage.setItem('theme', 'dark-mode');
+      });
   } else {
-    document.body.classList.add('light-mode'); // 기본적으로 라이트 모드로 설정
+      console.error('sun-icon 또는 moon-icon을 찾을 수 없습니다.');
   }
-
-  sunIcon.addEventListener('click', () => {
-    document.body.classList.remove('dark-mode');
-    document.body.classList.add('light-mode');
-    localStorage.setItem('theme', 'light-mode');
-  });
-
-  moonIcon.addEventListener('click', () => {
-    document.body.classList.remove('light-mode');
-    document.body.classList.add('dark-mode');
-    localStorage.setItem('theme', 'dark-mode');
-  });
 });
-``
