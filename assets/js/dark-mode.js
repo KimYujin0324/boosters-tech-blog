a7d5a8f63b98f5dd52f8e050248369e3e7878ade
KimYujin0324 <yujin.kim@boosters.kr>
2024-07-11 17:41:18 +0900

    feat: commit

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,35 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const sunIcon = document.querySelector('.theme-icon.sun');
   const moonIcon = document.querySelector('.theme-icon.moon');
-  const logo = document.getElementById('logo');
   const currentTheme = localStorage.getItem('theme');
 
-  // _config.yml에서 가져온 데이터
-  const lightLogo = "{{ site.baseurl }}/{{ site.logo }}";
-  const darkLogo = "{{ site.baseurl }}/{{ site.logo_dark }}";
-
-  // 초기 로고 설정
-  if (currentTheme === 'dark-mode') {
-      document.body.classList.add('dark-mode');
-      logo.src = darkLogo;
-  } else {
-      document.body.classList.add('light-mode');
-      logo.src = lightLogo;
+  if (currentTheme) {
+    document.body.classList.add(currentTheme);
   }
 
-  // 라이트 모드 버튼 클릭 이벤트
   sunIcon.addEventListener('click', () => {
-      document.body.classList.remove('dark-mode');
-      document.body.classList.add('light-mode');
-      localStorage.setItem('theme', 'light-mode');
-      logo.src = lightLogo; // 라이트 모드 로고
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light-mode');
   });
 
-  // 다크 모드 버튼 클릭 이벤트
   moonIcon.addEventListener('click', () => {
-      document.body.classList.remove('light-mode');
-      document.body.classList.add('dark-mode');
-      localStorage.setItem('theme', 'dark-mode');
-      logo.src = darkLogo; // 다크 모드 로고
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark-mode');
   });
 });
