26b37843f911cfea40ff89647b42c22f61a0e1a3
KimYujin0324 <yujin.kim@boosters.kr>
2024-07-11 16:33:58 +0900

    feat: commit

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -21,25 +21,23 @@
 //   });
 // });
 document.addEventListener('DOMContentLoaded', (event) => {
-  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+  const sunIcon = document.querySelector('.theme-icon.sun');
+  const moonIcon = document.querySelector('.theme-icon.moon');
   const currentTheme = localStorage.getItem('theme');
 
   if (currentTheme) {
     document.body.classList.add(currentTheme);
-
-    if (currentTheme === 'dark-mode') {
-      toggleSwitch.checked = true;
-    }
   }
 
-  toggleSwitch.addEventListener('change', (e) => {
-    if (e.target.checked) {
-      document.body.classList.add('dark-mode');
-      localStorage.setItem('theme', 'dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-      localStorage.setItem('theme', 'light-mode');
-    }
+  sunIcon.addEventListener('click', () => {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light-mode');
+  });
+
+  moonIcon.addEventListener('click', () => {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark-mode');
   });
 });
+
 
