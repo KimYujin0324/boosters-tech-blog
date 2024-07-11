9fefaf1fd5cf03076de5873cd37cba3df7a1f1e4
KimYujin0324 <yujin.kim@boosters.kr>
2024-07-11 16:03:24 +0900

    feat: commit

---
+++ assets/js/dark-mode.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+  const currentTheme = localStorage.getItem('theme');
+
+  if (currentTheme) {
+    document.body.classList.add(currentTheme);
+
+    if (currentTheme === 'dark-mode') {
+      toggleSwitch.checked = true;
+    }
+  }
+
+  toggleSwitch.addEventListener('change', (e) => {
+    if (e.target.checked) {
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('theme', 'dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+      localStorage.setItem('theme', 'light-mode');
+    }
+  });
+});
