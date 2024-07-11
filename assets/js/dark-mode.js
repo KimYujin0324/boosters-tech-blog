668ff004306c5703f7b272b55d8a411a4df09e2f
KimYujin0324 <yujin.kim@boosters.kr>
2024-07-11 16:38:05 +0900

    feat: commit

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -27,17 +27,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   if (currentTheme) {
     document.body.classList.add(currentTheme);
+  } else {
+    document.body.classList.add('light-mode'); // 기본적으로 라이트 모드로 설정
   }
 
   sunIcon.addEventListener('click', () => {
     document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
     localStorage.setItem('theme', 'light-mode');
   });
 
   moonIcon.addEventListener('click', () => {
+    document.body.classList.remove('light-mode');
     document.body.classList.add('dark-mode');
     localStorage.setItem('theme', 'dark-mode');
   });
 });
-
-
