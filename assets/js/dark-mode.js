1a0f69f1255179b6373f31c8bdae39307e576f95
KimYujin0324 <yujin.kim@boosters.kr>
2024-07-12 10:49:42 +0900

    feat: commit

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -18,37 +18,37 @@
 //   });
 // });
 document.addEventListener('DOMContentLoaded', (event) => {
-  const sunIcon = document.querySelector('.theme-icon.sun');
-  const moonIcon = document.querySelector('.theme-icon.moon');
-  const currentTheme = localStorage.getItem('theme');
+      const sunIcon = document.querySelector('.theme-icon.sun');
+      const moonIcon = document.querySelector('.theme-icon.moon');
+      const currentTheme = localStorage.getItem('theme');
 
-  const setActiveIcon = () => {
-    if (document.body.classList.contains('dark-mode')) {
-      sunIcon.classList.remove('active');
-      moonIcon.classList.add('active');
-    } else {
-      sunIcon.classList.add('active');
-      moonIcon.classList.remove('active');
-    }
-  };
+      const setActiveIcon = () => {
+        if (document.body.classList.contains('dark-mode')) {
+          sunIcon.classList.add('active');
+          moonIcon.classList.remove('active');
+        } else {
+          sunIcon.classList.remove('active');
+          moonIcon.classList.add('active');
+        }
+      };
 
-  if (currentTheme) {
-    document.body.classList.add(currentTheme);
-  }
+      if (currentTheme) {
+        document.body.classList.add(currentTheme);
+      }
 
-  setActiveIcon(); // 초기 아이콘 설정
+      setActiveIcon(); // 초기 아이콘 설정
 
-  sunIcon.addEventListener('click', () => {
-    document.body.classList.remove('dark-mode');
-    document.body.classList.add('light-mode');
-    localStorage.setItem('theme', 'light-mode');
-    setActiveIcon();
-  });
+      sunIcon.addEventListener('click', () => {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+        localStorage.setItem('theme', 'light-mode');
+        setActiveIcon();
+      });
 
-  moonIcon.addEventListener('click', () => {
-    document.body.classList.add('dark-mode');
-    document.body.classList.remove('light-mode');
-    localStorage.setItem('theme', 'dark-mode');
-    setActiveIcon();
-  });
-});
+      moonIcon.addEventListener('click', () => {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+        localStorage.setItem('theme', 'dark-mode');
+        setActiveIcon();
+      });
+    });
