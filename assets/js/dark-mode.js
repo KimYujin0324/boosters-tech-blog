9f8d1f1b35c17ac03995a768d7d40ed05498d296
KimYujin0324 <yujin.kim@boosters.kr>
2024-07-12 11:31:30 +0900

    feat: commit

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,54 +1,35 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-//   const sunIcon = document.querySelector('.theme-icon.sun');
-//   const moonIcon = document.querySelector('.theme-icon.moon');
-//   const currentTheme = localStorage.getItem('theme');
-
-//   if (currentTheme) {
-//     document.body.classList.add(currentTheme);
-//   }
-
-//   sunIcon.addEventListener('click', () => {
-//     document.body.classList.remove('dark-mode');
-//     localStorage.setItem('theme', 'light-mode');
-//   });
-
-//   moonIcon.addEventListener('click', () => {
-//     document.body.classList.add('dark-mode');
-//     localStorage.setItem('theme', 'dark-mode');
-//   });
-// });
 document.addEventListener('DOMContentLoaded', (event) => {
-      const sunIcon = document.querySelector('.theme-icon.sun');
-      const moonIcon = document.querySelector('.theme-icon.moon');
-      const currentTheme = localStorage.getItem('theme');
+    const sunIcon = document.querySelector('.theme-icon.sun');
+    const moonIcon = document.querySelector('.theme-icon.moon');
+    const currentTheme = localStorage.getItem('theme');
 
-      const setActiveIcon = () => {
-        if (document.body.classList.contains('dark-mode')) {
-          sunIcon.classList.remove('active');
-          moonIcon.classList.add('active');
-        } else {
-          sunIcon.classList.add('active');
-          moonIcon.classList.remove('active');
-        }
-      };
-
-      if (currentTheme) {
-        document.body.classList.add(currentTheme);
+    const setActiveIcon = () => {
+      if (document.body.classList.contains('dark-mode')) {
+        sunIcon.classList.remove('active');
+        moonIcon.classList.add('active');
+      } else {
+        sunIcon.classList.add('active');
+        moonIcon.classList.remove('active');
       }
+    };
 
-      setActiveIcon(); // 초기 아이콘 설정
+    if (currentTheme) {
+      document.body.classList.add(currentTheme);
+    }
 
-      sunIcon.addEventListener('click', () => {
-        document.body.classList.add('dark-mode');
-        document.body.classList.remove('light-mode');
-        localStorage.setItem('theme', 'dark-mode');
-        setActiveIcon();
-      });
+    setActiveIcon();
 
-      moonIcon.addEventListener('click', () => {
-        document.body.classList.remove('dark-mode');
-        document.body.classList.add('light-mode');
-        localStorage.setItem('theme', 'light-mode');
-        setActiveIcon();
-      });
+    sunIcon.addEventListener('click', () => {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
+      localStorage.setItem('theme', 'dark-mode');
+      setActiveIcon();
     });
+
+    moonIcon.addEventListener('click', () => {
+      document.body.classList.remove('dark-mode');
+      document.body.classList.add('light-mode');
+      localStorage.setItem('theme', 'light-mode');
+      setActiveIcon();
+    });
+});
